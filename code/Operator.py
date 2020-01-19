b5d1301b8656d320eb836eee2f91cde87f246d22
happylay <1803455421@qq.com>
2020-01-19 18:28:15 +0800

    添加for if 语句及三元表达式

--- code/Operator.py
+++ code/Operator.py
@@ -1,3 +1,5 @@
+import random
+
 name = 'happy'
 # id() 查看内存地址
 print(id(name),name)
@@ -32,3 +34,30 @@ print(a // b)
 
 # % 取余
 print(a % b)
+
+# 三元表达式：真结果 if 表达式 else 假结果
+a = 1
+b = 2
+result =(a+b) if a<b else (b-a)
+print(result)
+
+'''
+if else 语句
+'''
+if a==1:
+    print('真')
+else:
+    print('假')
+
+# 随机数
+print(random.randint(1,10))
+
+'''
+for 变量名 in 集合:
+    语句
+'''
+# 使用系统给定range()完成范围指定
+print(range(8))  # 包含0不包含8
+
+for i in range(8):
+    print(i)
